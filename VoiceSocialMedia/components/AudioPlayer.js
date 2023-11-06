7a717e11e87433fde38fd8d06816db4fc18b71ae
Artem <60270850+artemvandalovski@users.noreply.github.com>
2023-11-06 16:25:35 -0500

    done

--- VoiceSocialMedia/components/AudioPlayer.js
+++ VoiceSocialMedia/components/AudioPlayer.js
@@ -41,6 +41,7 @@ const AudioPlayer = ({ source }) => {
   const position = playbackStatus && playbackStatus.positionMillis;
 
   const formatTime = (time) => {
+    if (!time) return '0:00';
     const minutes = Math.floor(time / 60000);
     const seconds = ((time % 60000) / 1000).toFixed(0);
     return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
